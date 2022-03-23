fce62fd30fb75517d0cb86ad25ca6f2834446d0c
19991025 <100890013+19991025@users.noreply.github.com>
2022-03-23 15:20:26 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -1,13 +1,14 @@
-$(function () {
-    // topBanner:22.03.17 //  
-
+$(function(){
+    //////////////////////////////////
+    
+    // topBannner : lee 2022.03.17 수정
     function topBannerHandler() {
         // $('.topBannner').hide(); //display:none;
-        $('.topBanner').slideUp();
+        $('.topBannner').slideUp();
     }
-    $('.topBanner .container i').on('click', topBannerHandler);
-
-
+    $('.topBannner .container i').on('click', topBannerHandler);
+    
+    
     $('.visualSlide').slick({
         arrows: true,
         autoplay: true,
@@ -16,81 +17,82 @@ $(function () {
         prevArrow: '    <div class="msLeft"></div>      ',
         nextArrow: '    <div class="msRight"></div>      '
     });
-
-
+    
+    
     // 슬라이드에 애니메이션...
     $('.visualSlide figure').eq(1).addClass('oo');
-    $('.visualSlide').on('afterChange', function (e, s, c) {
-        $('.visualSlide figure').eq(c + 1).addClass('oo').siblings().removeClass('oo');
-        if (c === 1) {
+    $('.visualSlide').on('afterChange', function (e,s,c){
+        $('.visualSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
+        if (c===1) {
             $('.msLeft').addClass('oo')
         } else {
             $('.msLeft').removeClass('oo')
         }
     });
-
+    
     //
-
+    
     $('.eProductSlide').slick({
         arrows: false,
-        autoplay: true,
+        autoplay: true, 
         autoplaySpeed: 1000,
     });
-
-
-    $('.evenetProduct i:nth-of-type(1)').on('click', function () {
+    
+    
+    $('.evenetProduct i:nth-of-type(1)').on('click', function(){
         $('.eProductSlide').slick('slickPause')
     });
-
-    $('.evenetProduct i:nth-of-type(2)').on('click', function () {
+    
+    $('.evenetProduct i:nth-of-type(2)').on('click', function(){
         $('.eProductSlide').slick('slickPlay')
     });
-
+    
+    
     //
-
+    
     $('.aproductSlide').slick({
         arrows: false,
         dots: true,
         slidesToShow: 5,
         centerMode: true,
-        autoplay: true,
-        autoplaySpeed: 1000,
     });
-
-    $('.allProduct i:first-child').on('click', function () {
-        $('.aproductSlide').slick('slickNext')
-    });
-
-    $('.allProduct i:last-child').on('click', function () {
+    
+    $('.allProduct i:first-child').on('click', function(){
         $('.aproductSlide').slick('slickPrev')
     });
-
-    //ytplayer
-
-    $("#bgndVideo").YTPlayer({
-        videoURL: 'https://www.youtube.com/watch?v=qR13PjAwHwY',
-        containment: '.movieBg',
-        autoPlay: true,
-        mute: true,
-        startAt: 0,
-        opacity: 1,
-        showControls: false,
+    
+    $('.allProduct i:last-child').on('click', function(){
+        $('.aproductSlide').slick('slickNext')
     });
-
-    $('.movieBg i:first-child').on('click', function () {
-        $('#bgndVideo').YTPPause();
+    
+    
+    $('.popup01 button').on('click', function() {
+        $(this).parent().hide();
+    });    
+    
+    $('.toTop').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:0
+        });
+        return false;
     });
-    $('.movieBg i:last-child').on('click', function () {
-        $("#bgndVideo").YTPPlay();
+    
+    $(window).on('scroll', function (){
+        let sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(1000);
+        }
+        if (sct < 500 ) {
+            $('.toTop').fadeOut(1000);
+        }
     });
-
-    $('.tabMenu li').on('click', function () {
-        var idx = $(this).index();
-        $(this).addClass('oo').siblings().removeClass('oo');
-        $('.tabContent>div').eq($(this).index()).addClass('oo').siblings().removeClass('oo');
-    });
-
-
-
-    //////////console.log('play')////////////////////////
-})
+    
+    
+    
+    
+    
+    
+    
+    //////////////////////////////////
+    })
